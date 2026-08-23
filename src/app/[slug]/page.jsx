b9d2a023e04e37aa6e46c29577c33ea34831cd75
Gran Colinos <tienda@grancolinos.com>
2026-08-23 01:47:42 -0500

    feat(fonts): importar Cinzel y Montserrat con reglas CSS exactas

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -164,6 +164,16 @@ export default async function DynamicUniversalPage({ params }) {
         backgroundAttachment: bgImg ? 'fixed' : undefined,
       }}
     >
+      {/* Google Fonts: Cinzel & Montserrat */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600;700;800;900&family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      <style>{`
+        .font-serif {
+          font-family: 'Cinzel', serif !important;
+        }
+      `}</style>
+
       {/* Script Google Model-Viewer para visualización 3D / WebXR en vivo */}
       {has3DContent && (
         <Script 
